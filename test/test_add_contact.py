75904b84d58c8d18b0cb0a1ca99201af2ae88242
Mariia Shkirtil <straymonax01@gmail.com>
2022-04-23 00:41:18 +0300

    create classHelper - ContactHelper, link contact with sessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,12 +12,12 @@ def app(request):
 
     
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(name="Vasilii", patronymic="Petrovich", surname="Petrov",
-               nick="VasiliiParovoz", title="Vasilii Parovoz",
-               comp_name="OOO \"GoodPeopleComp\"", comp_address="The USA, Green str, apt 654",
-               home_number="8-800-999-45-45", mobile_number="+7(921)456-45-45",
-               work_number="8-800-555-55-55", fax="No",
-               email1="testVasili@mail.ru", email2="testVasili2@mail.com",
-               day_Birth="15", month_Birth="June", year_Birth="1975"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(name="Vasilii", patronymic="Petrovich", surname="Petrov",
+                               nick="VasiliiParovoz", title="Vasilii Parovoz",
+                               comp_name="OOO \"GoodPeopleComp\"", comp_address="The USA, Green str, apt 654",
+                               home_number="8-800-999-45-45", mobile_number="+7(921)456-45-45",
+                               work_number="8-800-555-55-55", fax="No",
+                               email1="testVasili@mail.ru", email2="testVasili2@mail.com",
+                               day_Birth="15", month_Birth="June", year_Birth="1975"))
+    app.session.logout()
