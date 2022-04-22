51b432f377cd51a7a868580d76590e9720c51bb1
Mariia Shkirtil <straymonax01@gmail.com>
2022-04-23 00:01:41 +0300

    create classHelper - GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="fgdfgdfgfd", header="dпавпвап", footer="вапвапвапва"))
+    app.group.create(Group(name="fgdfgdfgfd", header="dпавпвап", footer="вапвапвапва"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
