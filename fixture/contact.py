d1dd4c31c2b5eb81622336c4e117a7665e9b2d39
Mariia Shkirtil <straymonax01@gmail.com>
2022-04-23 16:11:34 +0300

    create new test - test_del_all_contacts

--- fixture/contact.py
+++ fixture/contact.py
@@ -65,8 +65,16 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        # select first group
+        # select first contact
         wd.find_element(by=By.NAME, value="selected[]").click()
+        # submit deletion
+        wd.find_element(by=By.XPATH, value="//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+
+    def delete_all_contacts(self):
+        wd = self.app.wd
+        # select all contacts
+        wd.find_element(by=By.ID, value="MassCB").click()
         # submit deletion
         wd.find_element(by=By.XPATH, value="//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
