95e7d6e82544246992cdb75db3253d5dcecaff03
Mariia Shkirtil <straymonax01@gmail.com>
2022-05-22 01:15:08 +0300

    1. create generator for contacts

---
+++ data/contacts.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(name='Vasilii', surname='Petrov', comp_name='Groupd+', home_number='00000000000'),
+    Contact(name='Petr', surname='Osipov', patronymic='Iosivofich', comp_address='Green Street')
+]
+
+
--- generator/contact.py
+++ generator/contact.py
@@ -1,11 +1,25 @@
 from model.contact import Contact
 import random
 import string
+import os.path
+import json
+import getopt
+import sys
 
-constant = [
-    Contact(name='Vasilii', surname='Petrov', comp_name='Groupd+', home_number='00000000000'),
-    Contact(name='Petr', surname='Osipov', patronymic='Iosivofich', comp_address='Green Street')
-]
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'n:f:', ['number of contacts', 'file'])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = 'data/contacts.json'
+
+for o, a in opts:
+    if o == '-n':
+        n = int(a)
+    elif o == '-f':
+        f = a
 
 
 def random_string(prefix, maxlen):
@@ -22,4 +36,10 @@ testdata = [Contact(name=random_string('name', 5), surname=random_string('surnam
                     comp_address=random_string('address', 10), comp_name=random_string('com_name', 7),
                     email1=random_string('email', 7), email2=random_string('email2', 7),
                     home_number=random_numbers('home', 11), work_number=random_numbers('work', 11))
-            for i in range(2)]
+            for i in range(n)]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', f)
+
+with open(file, 'w') as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
