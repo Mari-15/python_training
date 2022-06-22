13d23436f3719f6490fe8b8ed09be1aea9b3d33f
Mariia Shkirtil <straymonax01@gmail.com>
2022-06-22 19:46:50 +0300

    1. minor fix

--- test/test_add_all_contacts_to_group.py
+++ test/test_add_all_contacts_to_group.py
@@ -4,13 +4,15 @@ import random
 
 
 def test_add_all_contacts_to_group(app, db):
+    def clean(group):
+        return Group(name=group.name.strip(), header=group.header.strip(), footer=group.footer.strip())
     if len(db.get_contact_list()) <= 1:
         app.contact.create(Contact(name="Roland", surname="Braund"))
         app.contact.create(Contact(name="Voland", surname="Round"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name='Lost one'))
         app.navigation.return_to_homepage()
-    groups = db.get_group_list()
+    groups = list(map(clean, db.get_group_list()))
     group = random.choice(groups)
     app.navigation.return_to_homepage()
     app.contact.add_all_contacts_to_group(group_name=group.name)
