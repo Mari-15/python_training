5b5898274568d5f4c65442cd90b6e3170f76f5c5
Mariia Shkirtil <straymonax01@gmail.com>
2022-06-24 22:53:12 +0300

    1. fix test_add_contact_to_group_db.py

--- test/test_add_contact_to_group_db.py
+++ test/test_add_contact_to_group_db.py
@@ -11,10 +11,12 @@ def test_add_contact_to_group_db(app, db):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(name="Roland", surname="Braund"))
     if len(db.get_group_list()) == 0:
-        app.group.create(Group(name='Lost one'))
+        app.group.create(Group(name='Lost7one'))
         app.navigation.return_to_homepage()
     groups = db.get_group_list()
     group = random.choice(groups)
+    if len(db1.get_contacts_not_in_group(Group(number_of_group=group.number_of_group))) == 0:
+        app.contact.create(Contact(name="Land", surname="Und"))
     contacts = db1.get_contacts_not_in_group(Group(number_of_group=group.number_of_group))
     contact = random.choice(contacts)
     contact_in_group_old = db1.get_contacts_in_group(Group(number_of_group=group.number_of_group))
